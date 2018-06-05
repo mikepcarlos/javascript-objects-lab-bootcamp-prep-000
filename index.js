a4b2dce9aff5742b04c49ff5fbddbea11a61a4e3
Michael Carlos <michaelpcarlos@gmail.com>
2018-06-05 16:50:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
-function deleteFromObjectByKey(object, key, value) {
+function deleteFromObjectByKey(object, key) {
   delete object[key];
   return object;
 }
