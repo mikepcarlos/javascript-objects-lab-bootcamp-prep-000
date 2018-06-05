0369e7ad8ef38455834c2deea1838f4888eb5232
Michael Carlos <michaelpcarlos@gmail.com>
2018-06-05 16:47:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+const recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
